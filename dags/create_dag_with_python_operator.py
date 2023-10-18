86c91fec1c71ff2bae90fd0cbafb225e8b0ce7a0
David Hooton <david@hooton.org>
2023-10-18 18:58:38 +1100

    ROOT-app_gobbler_dags:    Updated dags/create_dag_with_python_operator.py

--- dags/create_dag_with_python_operator.py
+++ dags/create_dag_with_python_operator.py
@@ -12,9 +12,12 @@ def greet(name, age):
     print(f"Hello World! My name is {name}, "
           f"and I am {age} years old.")
 
+def get_name():
+    return 'Jerry'
+
 with DAG(
     default_args=default_args,
-    dag_id='our_dag_with_python_operator_v02',
+    dag_id='our_dag_with_python_operator_v03',
     description='Our DAG with Python Operator',
     start_date=datetime(2023, 10, 18, 5, 20),
     schedule_interval='@hourly'
@@ -25,4 +28,9 @@ with DAG(
         op_kwargs={'name': 'djh00t', 'age': 30}
     )
 
-    task1
+    task2 = PythonOperator(
+        task_id='get_name',
+        python_callable=greet
+    )
+
+    task1 >> task2
