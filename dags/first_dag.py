f8c5a4a82be449757846dc1a1da64361fab429ab
David Hooton <david@hooton.org>
2023-10-18 14:05:38 +1100

    first commit
    Signed-off-by: David Hooton <david@hooton.org>

---
+++ dags/first_dag.py
@@ -0,0 +1,21 @@
+from datetime import datetime, timedelta
+from airflow import DAG
+from airflow.operators.bash_operator import BashOperator
+
+default_args = {
+    'owner': 'djh00t',
+    'retries': 5,
+    'retry_delay': timedelta(minutes=1)
+}
+
+with DAG(
+    dag_id='first_dag',
+    default_args=default_args,
+    description='My first DAG',
+    start_date=datetime(2023, 10, 18, 14, 15),
+    scheduler_interval='@daily'
+) as dag:
+    task1 = BashOperator(
+        task_id='task1',
+        bash_command='echo "Hello from task1"'
+    )
