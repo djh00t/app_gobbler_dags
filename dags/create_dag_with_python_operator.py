e92f22a4885485a478b754d2469dd97e2e467a17
David Hooton <david@hooton.org>
2023-10-19 10:53:48 +1100

    ROOT-app_gobbler_dags:    Updated dags/create_dag_with_python_operator.py

--- dags/create_dag_with_python_operator.py
+++ dags/create_dag_with_python_operator.py
@@ -2,35 +2,53 @@ from datetime import datetime, timedelta
 from airflow import DAG
 from airflow.operators.python import PythonOperator
 
+
 default_args = {
-    'owner': 'djh00t',
+    'owner': 'coder2j',
     'retries': 5,
-    'retry_delay': timedelta(minutes=1)
+    'retry_delay': timedelta(minutes=5)
 }
 
-def greet(name, age):
-    print(f"Hello World! My name is {name}, "
-        f"and I am {age} years old.")
 
-def get_name():
-    return 'Jerry'
+def greet(some_dict, ti):
+    print("some dict: ", some_dict)
+    first_name = ti.xcom_pull(task_ids='get_name', key='first_name')
+    last_name = ti.xcom_pull(task_ids='get_name', key='last_name')
+    age = ti.xcom_pull(task_ids='get_age', key='age')
+    print(f"Hello World! My name is {first_name} {last_name}, "
+          f"and I am {age} years old!")
+
+
+def get_name(ti):
+    ti.xcom_push(key='first_name', value='Jerry')
+    ti.xcom_push(key='last_name', value='Fridman')
+
+
+def get_age(ti):
+    ti.xcom_push(key='age', value=19)
+
 
 with DAG(
     default_args=default_args,
-    dag_id='our_dag_with_python_operator_v05',
-    description='Our DAG with Python Operator',
-    start_date=datetime(2023, 10, 18, 5, 20),
-    schedule_interval='@hourly'
+    dag_id='our_dag_with_python_operator_v07',
+    description='Our first dag using python operator',
+    start_date=datetime(2021, 10, 6),
+    schedule_interval='@daily'
 ) as dag:
-#     task1 = PythonOperator(
-#         task_id='greet',
-#         python_callable=greet,
-#         op_kwargs={'name': 'djh00t', 'age': 30}
-#     )
+    task1 = PythonOperator(
+        task_id='greet',
+        python_callable=greet,
+        op_kwargs={'some_dict': {'a': 1, 'b': 2}}
+    )
 
     task2 = PythonOperator(
         task_id='get_name',
         python_callable=get_name
     )
 
-    task2
+    task3 = PythonOperator(
+        task_id='get_age',
+        python_callable=get_age
+    )
+
+    [task2, task3] >> task1
