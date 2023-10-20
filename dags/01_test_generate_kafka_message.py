4aeed4f7ddc841ca108c1d0fe78448b5e59b888b
David Hooton <david@hooton.org>
2023-10-21 10:23:57 +1100

    aider: Added code to create an Airflow DAG that generates a Kafka message with the specified attributes and sends it to the `normalize` topic.
    # Aider chat conversation:
    
    USER: Create an airflow DAG that generates a kafka message with the following attributes: topic:  normalize
    header: {
        "subject": "normalize-file-name",
        "version": "1.0",
        "status": "new"
    }
    body:
    {
        "file-name": "s3://fsg-gobbler/development/raw/2023/07/[
        Moiz]_2549-+61362705460_20230705035512(7873).wav",
        "last-action": "create",
        "next-action": "normalize-file-name"
    }
    
    ASSISTANT: To create an Airflow DAG that generates a Kafka message with the specified attributes, we need to modify the `01_test_generate_kafka_message.py` file.
    
    We will need to import the necessary libraries and define the DAG. Then, we will create a Python function that generates the Kafka message and wrap it in a PythonOperator.
    
    Here is the code change:
    
    ```python
    dags/01_test_generate_kafka_message.py
    <<<<<<< HEAD
    
    =======
    from airflow import DAG
    from airflow.operators.python_operator import PythonOperator
    from datetime import datetime, timedelta
    from kafka import KafkaProducer
    import json
    
    default_args = {
        'owner': 'airflow',
        'depends_on_past': False,
        'start_date': datetime(2021, 1, 1),
        'email': ['airflow@example.com'],
        'email_on_failure': False,
        'email_on_retry': False,
        'retries': 1,
        'retry_delay': timedelta(minutes=5),
    }
    
    def generate_kafka_message():
        producer = KafkaProducer(bootstrap_servers='localhost:9092')
        message = {
            "header": {
                "subject": "normalize-file-name",
                "version": "1.0",
                "status": "new"
            },
            "body": {
                "file-name": "s3://fsg-gobbler/development/raw/2023/07/[Moiz]_2549-+61362705460_20230705035512(7873).wav",
                "last-action": "create",
                "next-action": "normalize-file-name"
            }
        }
        producer.send('normalize', json.dumps(message))
        producer.flush()
    
    dag = DAG('test_generate_kafka_message', default_args=default_args, schedule_interval=timedelta(1))
    
    t1 = PythonOperator(
        task_id='generate_kafka_message',
        python_callable=generate_kafka_message,
        dag=dag)
    >>>>>>> updated
    ```
    
    This code creates a new DAG named `test_generate_kafka_message` that runs once a day. The DAG has a single task that generates a Kafka message with the specified attributes and sends it to the `normalize` topic. The Kafka producer is assumed to be running on `localhost:9092`.
    
    Please replace `'localhost:9092'` with the address of your Kafka server if it's running on a different host or port.

---
+++ dags/01_test_generate_kafka_message.py
@@ -0,0 +1,40 @@
+from airflow import DAG
+from airflow.operators.python_operator import PythonOperator
+from datetime import datetime, timedelta
+from kafka import KafkaProducer
+import json
+
+default_args = {
+    'owner': 'airflow',
+    'depends_on_past': False,
+    'start_date': datetime(2021, 1, 1),
+    'email': ['airflow@example.com'],
+    'email_on_failure': False,
+    'email_on_retry': False,
+    'retries': 1,
+    'retry_delay': timedelta(minutes=5),
+}
+
+def generate_kafka_message():
+    producer = KafkaProducer(bootstrap_servers='localhost:9092')
+    message = {
+        "header": {
+            "subject": "normalize-file-name",
+            "version": "1.0",
+            "status": "new"
+        },
+        "body": {
+            "file-name": "s3://fsg-gobbler/development/raw/2023/07/[Moiz]_2549-+61362705460_20230705035512(7873).wav",
+            "last-action": "create",
+            "next-action": "normalize-file-name"
+        }
+    }
+    producer.send('normalize', json.dumps(message))
+    producer.flush()
+
+dag = DAG('test_generate_kafka_message', default_args=default_args, schedule_interval=timedelta(1))
+
+t1 = PythonOperator(
+    task_id='generate_kafka_message',
+    python_callable=generate_kafka_message,
+    dag=dag)
