c87e4e8fbb411035bf93c6ba3a4b25e71b56c676
David Hooton <david@hooton.org>
2023-10-24 17:59:00 +1100

    ROOT-app_gobbler_dags:    Updated dags/02_normalize_file.py

--- dags/02_normalize_file.py
+++ dags/02_normalize_file.py
@@ -1,13 +1,14 @@
 from datetime import datetime, timedelta
+from airflow.utils.dates import days_ago
 from airflow import DAG, settings
 from airflow.operators.python import PythonOperator
 from airflow.sensors.base_sensor_operator import BaseSensorOperator
 from airflow.models import XCom
 from sqlalchemy import and_, or_
-import json
+import re
 
 # Set variables
-VERSION='v1.0.0f'
+VERSION='v1.0.0g'
 
 # Function to echo "GO TIME"
 def echo_go_time(**kwargs):
@@ -17,34 +18,37 @@ def echo_go_time(**kwargs):
 class CustomXComSensor(BaseSensorOperator):
     def poke(self, context):
         session = settings.Session()
-        dag_id = context['dag'].dag_id  # Get the dag_id from the context
 
-        # Query for 'taskID'
-        query_taskID = session.query(XCom).filter(
+        # Query for 'goTime' with value "OK" and specific dag_id pattern
+        query_goTime = session.query(XCom).filter(
             and_(
-                XCom.dag_id == dag_id,
-                XCom.key == 'taskID'
+                XCom.dag_id.like('01_normalize_kafka_listener_%'),
+                XCom.key == 'goTime',
+                XCom.value == b'"OK"'  # Serialized string in XCom is bytes
             )
         ).first()
 
-        # Query for 'goTime'
-        query_goTime = session.query(XCom).filter(
+        # Query for 'taskID' with specific dag_id pattern
+        query_taskID = session.query(XCom).filter(
             and_(
-                XCom.dag_id == dag_id,
-                XCom.key == 'goTime'
+                XCom.dag_id.like('01_normalize_kafka_listener_%'),
+                XCom.key == 'taskID'
             )
         ).first()
 
         session.close()
 
-        # Check if both 'taskID' and 'goTime' exist
-        return query_taskID is not None and query_goTime is not None
+        # Check if both 'taskID' and 'goTime' exist and additional conditions
+        return (query_taskID is not None and re.fullmatch(r'[a-fA-F0-9]{28}', query_taskID.value.decode('utf-8'))) and \
+               (query_goTime is not None and query_goTime.value == b'"OK"')
+
 
 
 # Define default_args dictionary
 default_args = {
     'owner': 'airflow',
     'depends_on_past': False,
+    'start_date': days_ago(1),
     'email_on_failure': False,
     'email_on_retry': False,
     'retries': 1,
@@ -56,7 +60,7 @@ dag = DAG(
     '02_normalize_file_' + VERSION,
     default_args=default_args,
     description='File normalization DAG',
-    schedule_interval=None,  # Overridden at trigger time
+    schedule_interval=timedelta(minutes=1),
     start_date=datetime(2023, 1, 1),
     catchup=False,
     tags=['normalize', 'file', 'gobbler'],
